8aa1c50d15794f26c7702c741123584821e748b0
Atharva <atharvasharma.cs25@gmail.com>
2025-02-19 22:54:07 +0530

    [Atharva Sharma] Add. implemented and Tested functionality of list to Json

---
+++ src/main/java/com/ioprogramming/handsonpractise/listtojson/Person.java
@@ -0,0 +1,22 @@
+package com.ioprogramming.handsonpractise.listtojson;
+
+import org.json.JSONObject;
+
+// Java Bean representing a Person
+class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Convert this Person object into a JSONObject
+    public JSONObject toJson() {
+        JSONObject json = new JSONObject();
+        json.put("name", name);
+        json.put("age", age);
+        return json;
+    }
+}
